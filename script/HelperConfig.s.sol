c05ba64fda9d54cb0406c0b9464a52ec89ee43a6
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-25 12:45:12 -0500

    feat: update script to deploy a Mock Chainlink VRF

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,8 +2,15 @@
 pragma solidity 0.8.19;
 
 import {Script} from 'forge-std/Script.sol';
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstants {
+    /* VRF Mock Values */
+    uint96 public MOCK_BASE_FEE = 0.25 ether;
+    uint96 public MOCK_GAS_PRICE_LINK = 1e9;
+    // LINK / ETH price
+    int256 public MOCK_WEI_PER_UINT_LINK = 4e15;
+
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
@@ -30,10 +37,10 @@ contract HelperConfig is CodeConstants, Script {
     function getConfigByChainId(uint256 chainId) public returns(NetworkConfig memory){
         if(networkConfigs[chainId].vrfCoordinator != address(0)){
             return networkConfigs[chainId];
-        } else if (chainId = LOCAL_CHAIN_ID){
-            //getOrCreateAnvilEthConfig()
+        } else if (chainId == LOCAL_CHAIN_ID){
+            return getOrCreateAnvilEthConfig();
         }else {
-            revert HelperConfig__InvalidChainId()
+            revert HelperConfig__InvalidChainId();
         }
     }
 
@@ -52,5 +59,25 @@ contract HelperConfig is CodeConstants, Script {
         if(localNetworkConfig.vrfCoordinator != address(0)){
             return localNetworkConfig;
         }
+
+        // Deploy mocks and such
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE
+            ,MOCK_GAS_PRICE_LINK
+            ,MOCK_WEI_PER_UINT_LINK
+        );
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({entranceFee: 0.01 ether //10000000000000000
+            ,interval: 30 // 30 seconds
+            ,vrfCoordinator: address(vrfCoordinatorMock)
+            //rest doesn't matter
+            ,gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae
+            ,callbackGasLimit: 500000 //500,000 gas
+            ,subscriptionId: 0 // might have to fix this
+        });
+
+        return localNetworkConfig;
     }
 }
