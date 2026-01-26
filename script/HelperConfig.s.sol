44e0c0b6266637832c3821ef66153375429b4771
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-26 11:07:04 -0500

    feat: fund subscription PART II

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import {Script} from 'forge-std/Script.sol';
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from 'test/mocks/LinkToken.sol';
 
 abstract contract CodeConstants {
     /* VRF Mock Values */
@@ -25,6 +26,7 @@ contract HelperConfig is CodeConstants, Script {
         bytes32 gasLane;
         uint32 callbackGasLimit;
         uint256 subscriptionId;
+        address link;
     }
 
     NetworkConfig public localNetworkConfig;
@@ -55,6 +57,7 @@ contract HelperConfig is CodeConstants, Script {
             ,gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae
             ,callbackGasLimit: 500000 //500,000 gas
             ,subscriptionId: 0
+            ,link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
         });
     }
 
@@ -71,6 +74,7 @@ contract HelperConfig is CodeConstants, Script {
             ,MOCK_GAS_PRICE_LINK
             ,MOCK_WEI_PER_UINT_LINK
         );
+        LinkToken linkToken = new LinkToken();
         vm.stopBroadcast();
 
         localNetworkConfig = NetworkConfig({entranceFee: 0.01 ether //10000000000000000
@@ -80,6 +84,7 @@ contract HelperConfig is CodeConstants, Script {
             ,gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae
             ,callbackGasLimit: 500000 //500,000 gas
             ,subscriptionId: 0 // might have to fix this
+            ,link: address(linkToken)
         });
 
         return localNetworkConfig;
